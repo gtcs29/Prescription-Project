0646abc27574ff604129f9ab48d8f75fbf3a0f3a
Gitika Bose <gb2606@columbia.edu>
2018-07-22 13:25:49 -0400

    initial app design

---
+++ prescription-organizer/navigation/AppNavigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+
+import { Container, Button, Text, Content, Form, Item, Label, Input, Header, Body, Title} from 'native-base';
+
+import MainTabNavigator from './MainTabNavigator';
+
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+import SignInScreen from '../screens/SignInScreen';
+
+
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    // App: AppStack,
+    Main: MainTabNavigator,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+);
